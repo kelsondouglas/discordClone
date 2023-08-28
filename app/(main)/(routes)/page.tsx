c4675e74d5cfb4fdc003517e9eecaaca2014b722
Kelson Douglas <kelsondouglas23@hotmail.com>
2023-08-28 10:17:41 -0300

    fix: adding .env to .gitignore

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <p className="text-3xl font-bold text-indigo-500">Hello discord clone</p>
-      <Button>Button</Button>
-    </div>
-  );
+  return <div>This is a protected route.</div>;
 }
