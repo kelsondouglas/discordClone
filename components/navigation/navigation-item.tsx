6d26c3f67c69ea4124b3737acda6908f4a7d4ef5
Kelson Douglas <kelsondouglas23@hotmail.com>
2023-09-01 10:20:00 -0300

    fix: correct spacing and add onClick to nav item

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -18,7 +18,12 @@ const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
 
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button
+        onClick={() => {
+          router.push(`/servers/${id}`);
+        }}
+        className="group relative flex items-center"
+      >
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
